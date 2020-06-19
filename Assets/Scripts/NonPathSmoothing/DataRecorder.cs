eaa6fd8d78ed357be244fb215ab637d48bcb219f
Steven Lilley <steven.b.lilley@gmail.com>
2020-06-19 16:21:45 -0400

    Created classes DataRecorder and PathData to handle the data we would like to archive and record from that pathing of the agents throughout a demonstration. These will also deal with outputting that data in a readable format.

---
+++ Assets/Scripts/NonPathSmoothing/DataRecorder.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataRecorder
+{
+    private static DataRecorder instance;
+    private DataRecorder()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+
+        instance = this;
+    }
+    public static DataRecorder Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                new DataRecorder();
+            }
+            return instance;
+        }
+    }
+
+    private static List<PathData> pathRecords = new List<PathData>();
+    private static PathData currentPath;
+
+    /*
+     * Add a pathData element to the records
+     */
+    public void AddPathRecord(PathData pathData)
+    {
+        pathRecords.Add(pathData);
+    }
+
+    /*
+     * Sets the current path to determine which PathData to currently be adding data to
+     */
+    public void SetCurrentPath(PathData path)
+    {
+        currentPath = path;
+    }
+
+    /*
+     * Adds the finished currentPath to the pathRecords after filling all the data in a path
+     */
+    public void AddCompletedPathRecord()
+    {
+        pathRecords.Add(currentPath);
+    }
+
+    /*
+     * Initializes a new PathData object as the starting point to fill with PathData information
+     */
+    public void GenerateNewPathData()
+    {
+        currentPath = new PathData();
+        Debug.Log("Path created in recorder.");
+    }
+
+    public void SetCurrentPathAgent(UnitSimple agent)
+    {
+        currentPath.SetAgentData(agent);
+    }
+
+    public void SetCurrentPathPath(Node[] path)
+    {
+        currentPath.SetNodePath(path);
+    }
+
+    public void SetCurrentPathSpawn(Transform spawnPoint)
+    {
+        currentPath.SetSpawnPoint(spawnPoint);
+    }
+
+    public void OutputPathData()
+    {
+        int identifier = 0;
+
+        foreach (PathData path in pathRecords)
+        {
+            string pathInformation = "";
+            foreach (Node node in path.GetNodePath())
+            {
+                pathInformation += node.NodeCoordinates + "\n";
+            }
+
+            Debug.Log("Agent ID: " + identifier + "\n" +
+                "Spawned At: " + path.GetSpawnPoint().name + "\n" +
+                "Agent's Window Affinity: " + path.GetAgentData().Window + "\n" +
+                "Agent's Connectivity Affinity: " + path.GetAgentData().Connectivity + "\n" +
+                "Path List: " + pathInformation);
+
+            identifier++;
+        }
+    }
+}
---
+++ Assets/Scripts/NonPathSmoothing/PathData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathData
+{
+    // Contains useful data to track for every path of each agent
+    private UnitSimple agentData;
+    private Node[] nodePath;
+    private Transform spawnPoint;
+
+    public void SetAgentData(UnitSimple agent)
+    {
+        agentData = agent;
+    }
+
+    public UnitSimple GetAgentData()
+    {
+        return agentData;
+    }
+
+    public void SetNodePath(Node[] path)
+    {
+        nodePath = path;
+    }
+
+    public Node[] GetNodePath()
+    {
+        return nodePath;
+    }
+
+    public void SetSpawnPoint(Transform location)
+    {
+        spawnPoint = location;
+    }
+
+    public Transform GetSpawnPoint()
+    {
+        return spawnPoint;
+    }
+
+}
