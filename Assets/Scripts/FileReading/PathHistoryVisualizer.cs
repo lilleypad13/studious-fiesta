5a4629633d7569a22d3d4c4ed4b846b757dc8a67
Steven Lilley <steven.b.lilley@gmail.com>
2020-06-29 19:25:13 -0400

    Added to create a dropdown UI which could visualize past paths created by agents within the current play session

---
+++ Assets/Scripts/FileReading/PathHistoryVisualizer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathHistoryVisualizer : MonoBehaviour
+{
+    private DataRecorder dataRecorder;
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        //dataRecorder = GetComponent<DataRecorder>();
+        dataRecorder = DataRecorder.Instance;
+        lineRenderer = GetComponent<LineRenderer>();
+
+        Debug.Log("PathHistoryVisualizer got DataRecorder reference: " + dataRecorder.gameObject.name);
+        Debug.Log("PathHistoryVisuzlier got Linerenderer reference: " + lineRenderer.name);
+    }
+
+    /*
+     * Informs LineRenderer how to draw a line between all the different node's world positions within 
+     * an entire path obtained from the DataRecorder pathRecords.
+     */
+    public void VisualizePath(int index)
+    {
+        PathData pathData = dataRecorder.GetPathRecord(index);
+
+        if (pathData == null)
+            Debug.Log("Path Visualizer failed to obtain pathData");
+
+        Node[] path = pathData.GetNodePath();
+        List<Vector3> pathPositions = new List<Vector3>();
+
+        if(path == null)
+            Debug.Log("Path Visualizer failed to obtain path");
+
+        foreach (Node node in path)
+        {
+            pathPositions.Add(node.worldPosition);
+        }
+
+        Vector3[] pathPositionsArray = pathPositions.ToArray();
+
+        lineRenderer.positionCount = pathPositionsArray.Length;
+        lineRenderer.SetPositions(pathPositionsArray);
+    }
+}
---
+++ Assets/Scripts/UI/DropdownPathHistory.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownPathHistory : MonoBehaviour
+{
+    public Text textBox;
+
+    public PathHistoryVisualizer pathVisualizer;
+
+    private Dropdown dropdown;
+    private int pathCounter;
+
+    private void Start()
+    {
+        dropdown = transform.GetComponent<Dropdown>();
+
+        pathCounter = 0;
+        dropdown.options.Clear();
+
+        //List<string> items = new List<string>();
+        //items.Add("Item 1");
+        //items.Add("Items 2");
+
+        //// Fill dropdown with items
+        //// This populates the dropdown with the strings found in items
+        //foreach (string item in items)
+        //{
+        //    dropdown.options.Add(new Dropdown.OptionData() { text = item });
+        //}
+
+        //DropdownItemSelected(dropdown);
+
+        // Adds the method DropdownItemSelected with this parameter to the onValueChanged delegate
+        // Makes it so that when onValueChanged Unity event is invoked with this dropdown, 
+        // DropdownItemSelected is also called
+        dropdown.onValueChanged.AddListener(delegate { DropdownItemSelected(dropdown); });
+    }
+
+    /*
+     * Actions to perform when the dropdown value changes (a dropdown item is selected)
+     */
+    private void DropdownItemSelected(Dropdown dropdown)
+    {
+        // ADD VISUALIZATION INITIATION HERE
+        // Using the dropdown index, recall path data for the equivalent index in the path data history
+
+        int index = dropdown.value;
+        Debug.Log("Dropdown index is: " + index);
+
+        textBox.text = dropdown.options[index].text;
+        pathVisualizer.VisualizePath(index);
+    }
+
+    /*
+     * Add an element to this dropdown from an outside source while supplying the string 
+     * to be used as the text for that new dropdown element.
+     */
+    public void AddToDropdownList(string item)
+    {
+        dropdown.options.Add(new Dropdown.OptionData() { text = item });
+    }
+
+    /*
+     * Specifically used to add a new path to the dropdown list.
+     * Keeps track of the number of paths added to number them as it populates 
+     * the dropdown.
+     */
+    public void AddNewPathToDropdown()
+    {
+        string addedPathText = "Path " + pathCounter;
+        AddToDropdownList(addedPathText);
+        pathCounter++;
+    }
+}
