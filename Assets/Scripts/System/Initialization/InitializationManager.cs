e9cbb1f64a3110bd850f362147c52609b5a14c28
Steven Lilley <steven.b.lilley@gmail.com>
2020-11-02 17:11:42 -0500

    Added Initialization system to major classes to help with their order of operations upon Awake

---
+++ Assets/Scripts/System/Initialization/InitializationManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializationManager : MonoBehaviour
+{
+    [SerializeField]private Initializer[] initializingClasses;
+
+    private void Awake()
+    {
+        foreach (Initializer initializer in initializingClasses)
+        {
+            initializer.Initialization();
+            DebugCurrentInitializationStep(initializer);
+        }
+    }
+
+    #region Debug Methods
+
+    private int initializationStep = 0;
+
+    public void DebugCurrentInitializationStep(Initializer currentInitializer)
+    {
+        Debug.Log($"Step {initializationStep}: {currentInitializer.GetType()} initialized.");
+        initializationStep++;
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/System/Initialization/Initializer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Initializer : MonoBehaviour
+{
+    public abstract void Initialization();
+}
