281651aa64a4e07acdb724e0d1e570b6c512573c
Steven Lilley <steven.b.lilley@gmail.com>
2020-11-05 18:00:48 -0500

    Renamed ComponentApplicator to InfluenceApplicator; Added additional information to pass on to created Influence components

---
+++ Assets/Scripts/Architecture/InfluenceApplicator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TypeReferences;
+using UnityEngine;
+
+[Serializable]
+public class InfluenceApplicator : Applicator
+{
+    [Inherits(typeof(Influence))]
+    [SerializeField] private TypeReference influenceComponentToApply;
+    public TypeReference InfluenceComponentToApply
+    {
+        get => influenceComponentToApply;
+        set { influenceComponentToApply = value; }
+    }
+
+    [SerializeField] private int influenceAmount;
+    public int InfluenceAmount
+    {
+        get => influenceAmount;
+        set
+        {
+            if (value >= MathArchCost.Instance.MIN_ARCHVALUE)
+            {
+                if (value <= MathArchCost.Instance.MAX_ARCHVALUE)
+                    influenceAmount = value;
+                else
+                    influenceAmount = MathArchCost.Instance.MAX_ARCHVALUE;
+            }
+            else
+                influenceAmount = MathArchCost.Instance.MIN_ARCHVALUE;
+        }
+    }
+}
