e6847e735705a62b127af5c0cdddada0781f31eb
Steven Lilley <steven.b.lilley@gmail.com>
2020-11-06 19:22:52 -0500

    Created ColliderCreator to automate applying convex meshes to objects; PrepareModelForNavigation class takes in the model and applies the colliders to all the children now

---
+++ Assets/Scripts/System/ColliderCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderCreator
+{
+    public void CreateColliders(List<GameObject> objectsToApplyCollidersTo)
+    {
+        foreach (GameObject item in objectsToApplyCollidersTo)
+        {
+            item.AddComponent<MeshCollider>();
+            item.GetComponent<MeshCollider>().convex = true;
+        }
+    }
+}
---
+++ Assets/Scripts/System/PrepareModelForNavigation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrepareModelForNavigation : Initializer
+{
+    [SerializeField] GameObject modelToPrepare;
+    private ChildObjectGatherer childGatherer = new ChildObjectGatherer();
+    public ChildObjectGatherer ChildGatherer
+    {
+        get => childGatherer;
+    }
+
+    private ColliderCreator colliderCreator = new ColliderCreator();
+
+    public override void Initialization()
+    {
+        childGatherer.CreateListOfChildrenObjects(modelToPrepare);
+        colliderCreator.CreateColliders(childGatherer.AllObjectChildren);
+    }
+}
