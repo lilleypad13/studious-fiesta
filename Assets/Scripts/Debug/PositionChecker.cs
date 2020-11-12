793d6ba567bfdfa9319ffd388930680990b70000
Steven Lilley <steven.b.lilley@gmail.com>
2020-11-12 18:16:54 -0500

    Script to debug the position of an object in various ways

---
+++ Assets/Scripts/Debug/PositionChecker.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PositionChecker : MonoBehaviour
+{
+    private void Start()
+    {
+        DebugWorldPosition(this.gameObject);
+        DebugLocalPosition(this.gameObject);
+        DebugBoundsCenterPosition(this.gameObject);
+    }
+
+    public void DebugWorldPosition(GameObject objectToCheck)
+    {
+        Vector3 position = objectToCheck.transform.position;
+
+        Debug.Log($"{objectToCheck.name} is located at world position {position}.");
+    }
+
+    public void DebugLocalPosition(GameObject objectToCheck)
+    {
+        Vector3 position = objectToCheck.transform.localPosition;
+
+        Debug.Log($"{objectToCheck.name} is located at local position {position}.");
+    }
+
+    public void DebugBoundsCenterPosition(GameObject objectToCheck)
+    {
+        Renderer renderer;
+        renderer = objectToCheck.GetComponent<Renderer>();
+
+        if (renderer != null)
+        {
+            Vector3 position = renderer.bounds.center;
+
+            Debug.Log($"{objectToCheck.name} has its bounds center at {position}.");
+        }
+        else
+            Debug.Log($"{this.name} did not find a Renderer component on {objectToCheck.name}.");
+    }
+}
