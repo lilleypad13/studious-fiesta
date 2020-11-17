49394cf1390bf35bab7a03134b3c20dca9fef1ec
Steven Lilley <steven.b.lilley@gmail.com>
2020-11-17 18:11:46 -0500

    Added an overall class to hold persistent data about the model at hand that may be useful for any class globally to be able to access

---
+++ Assets/Scripts/System/GlobalModelData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GlobalModelData
+{
+    private static GlobalModelData instance = null;
+    private static readonly object padlock = new object();
+
+    public static GlobalModelData Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                    instance = new GlobalModelData();
+            }
+            return instance;
+        }
+    }
+
+    private Bounds modelBounds;
+    public Bounds ModelBounds
+    {
+        get => modelBounds;
+        set => modelBounds = value;
+    }
+}
