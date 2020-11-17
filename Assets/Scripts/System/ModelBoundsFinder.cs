fc8f59e3d321e4e91803b5614e86e12ed669b1f0
Steven Lilley <steven.b.lilley@gmail.com>
2020-11-17 18:12:56 -0500

    Created a new class and utilized it through the PrepareModelForNavigation class to obtain information on the bounds of the entire group of models within the overal architecture model and pass that data to the new GlobalModelData class.

---
+++ Assets/Scripts/System/ModelBoundsFinder.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ModelBoundsFinder
+{
+    public static Bounds EncapsulateGroupOfModelRenderers(List<GameObject> groupOfModels)
+    {
+        Bounds combinedBounds = groupOfModels[0].GetComponent<Renderer>().bounds;
+
+        foreach (GameObject model in groupOfModels)
+        {
+            combinedBounds.Encapsulate(model.GetComponent<Renderer>().bounds);
+        }
+
+        return combinedBounds;
+    }
+
+    public static string ReportModelBounds(Bounds modelBounds)
+    {
+        Bounds bounds = modelBounds;
+
+        string boundsMessage = "===Model Bounds Report===\n";
+
+        boundsMessage += $"Center: {bounds.center}\n";
+        boundsMessage += $"Extents: {bounds.extents}\n";
+        boundsMessage += $"Max: {bounds.max}\n";
+        boundsMessage += $"Min: {bounds.min}\n";
+        boundsMessage += $"Size: {bounds.size}\n";
+        boundsMessage += $"X: {bounds.size.x}\n";
+        boundsMessage += $"Y: {bounds.size.y}\n";
+        boundsMessage += $"Z: {bounds.size.z}\n";
+
+        return boundsMessage;
+    }
+
+    public static void ReportListOfBounds(List<GameObject> modelList)
+    {
+        Renderer rendererToCheck;
+        foreach (GameObject model in modelList)
+        {
+            rendererToCheck = model.GetComponent<Renderer>();
+            if (rendererToCheck != null)
+                Debug.Log($"{model.name} Bounds Report:\n" + ReportModelBounds(rendererToCheck.bounds));
+        }
+    }
+
+    #region Debugging
+    public static void VisualizeBoundsBox(Bounds bounds)
+    {
+        OnDrawGizmos(bounds);
+    }
+
+    private static void OnDrawGizmos(Bounds bounds)
+    {
+        Gizmos.color = Color.yellow;
+        if (bounds.size != Vector3.zero)
+            Gizmos.DrawWireCube(bounds.center, bounds.size);
+    }
+    #endregion
+}
--- Assets/Scripts/System/PrepareModelForNavigation.cs
+++ Assets/Scripts/System/PrepareModelForNavigation.cs
@@ -6,16 +6,27 @@ public class PrepareModelForNavigation : Initializer
 {
     [SerializeField] GameObject modelToPrepare;
     private ChildObjectGatherer childGatherer = new ChildObjectGatherer();
-    public ChildObjectGatherer ChildGatherer
-    {
-        get => childGatherer;
-    }
-
+    public ChildObjectGatherer ChildGatherer { get => childGatherer; }
     private ColliderCreator colliderCreator = new ColliderCreator();
+    private GlobalModelData globalModelData;
 
+    Bounds entireModelBounds;
+
+    private void OnDrawGizmos()
+    {
+        ModelBoundsFinder.VisualizeBoundsBox(entireModelBounds);
+    }
     public override void Initialization()
     {
+        globalModelData = GlobalModelData.Instance;
+
         childGatherer.CreateListOfChildrenObjects(modelToPrepare);
         colliderCreator.CreateColliders(childGatherer.AllObjectChildren);
+
+        entireModelBounds = ModelBoundsFinder.EncapsulateGroupOfModelRenderers(childGatherer.AllObjectChildren);
+        Debug.Log(ModelBoundsFinder.ReportModelBounds(entireModelBounds));
+        ModelBoundsFinder.ReportListOfBounds(childGatherer.AllObjectChildren);
+
+        globalModelData.ModelBounds = entireModelBounds;
     }
 }
