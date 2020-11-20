3bd71ead3eb2bb4683d466ea3f5c650a27bd3bc2
Steven Lilley <steven.b.lilley@gmail.com>
2020-11-20 17:39:48 -0500

    Editing CSV reader to make it more consistent and automated for reading in data

--- Assets/Scripts/FileReading/CSVReader.cs
+++ Assets/Scripts/FileReading/CSVReader.cs
@@ -1,9 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.IO;
 using UnityEngine;
 
-public class CSVReader: MonoBehaviour
+public class DataWithPosition
+{
+    int xPosition;
+    int zPosition;
+    int dataValue;
+
+    public DataWithPosition(int x, int z, int data)
+    {
+        xPosition = x;
+        zPosition = z;
+        dataValue = data;
+    }
+
+}
+
+public class CSVReader : MonoBehaviour
 {
     // Data about the file being read
     [Header("File to Read")]
@@ -13,11 +29,11 @@ public class CSVReader: MonoBehaviour
     [Header("File Parameters")]
     [Tooltip("EXPERIMENTAL: Generally keep at 1 (unless weird data set).")]
     [SerializeField] private int dataIncrement = 1; // Increment value between coordinates of each data point
-    [SerializeField] private int dataHeight = 90; // number of data points in the z-direction
-    [SerializeField] private int dataWidth = 90; // number of data points in the x-direction 
     // dataWidth could be automated by finding the largest x-value
     [Tooltip("Value to assign empty coordinate locations not found in the file.")]
     [SerializeField] private int defaultValueWhenNotFound = 0;
+
+    [SerializeField] private bool convertInchesToFeet = false;
 
     // Data to hold for use within Unity
     private static CSVReader instance;
@@ -42,13 +58,12 @@ public class CSVReader: MonoBehaviour
         }
     }
 
-    public int[,,] FullData
-    {
-        get { return fullData; }
-    }
-    private int[,,] fullData;
-
-    //private int dataCounter = 1;
+    //public int[,,] FullData
+    //{
+    //    get { return fullData; }
+    //}
+    //private int[,,] fullData;
+    private List<DataWithPosition> fullData = new List<DataWithPosition>();
 
     /*
      * Reads data from a text asset file and separates data into an organized array.
@@ -61,51 +76,66 @@ public class CSVReader: MonoBehaviour
         string[] data = gridData.text.Split(new char[] { '\n' }); // TODO: Check if this is giving +/- 1 extra piece of data
         string[] row = data[0].Split(new char[] { ',' }); // Counts number of elements in first row to determine how many columns data has
         int[] rowInt = new int[row.Length];
-        Debug.Log(data.Length);
-        Debug.Log(row.Length);
+        Debug.Log($"The number of rows in the data is: {data.Length} and the number of columns in the data is: {row.Length}.");
+        DebugListOutStringArray(data, "This is the data string array:");
 
-        fullData = new int[dataWidth, dataHeight, row.Length]; // Could possibly automate dataWidth value
-
-        for(int i = 0; i < dataWidth; i++)
+        while (data[dataCounter] != string.Empty)
         {
-            for (int j = 0; j < dataHeight; j++)
-            {
-                row = data[dataCounter].Split(new char[] { ',' });
+            DataWithPosition currentData;
 
-                // Converts row of string data from file into an int array
-                for (int k = 0; k < row.Length; k++)
-                {
-                    int.TryParse(row[k], out rowInt[k]);
-                }
+            string[] currentRow = data[dataCounter].Split(new char[] { ',' });
+            if(convertInchesToFeet)
+                currentData = new DataWithPosition(int.Parse(currentRow[1]) / 12, int.Parse(currentRow[2]) / 12, int.Parse(currentRow[3]) / 12);
+            else
+                currentData = new DataWithPosition(int.Parse(currentRow[1]), int.Parse(currentRow[2]), int.Parse(currentRow[3]));
 
-                // Checks if current data row in file has coordinates which matchup with an array element 
-                // in the generated data array.
-                // If it matches, it assigns that entire row's data to that row in the data array and moves the process 
-                // forward to assign the next row of data from the file.
-                // If they do not match, it creates a new row of data with the coordinates not found in the 
-                // read file and assigns it a designated default "not found" value, and retains the same row 
-                // of data until it finds where to place it.
-                if(rowInt[1] == (i + 1) * dataIncrement && rowInt[2] == (j + 1) * dataIncrement)
-                {
-                    for (int k = 0; k < row.Length; k++)
-                    {
-                        fullData[i, j, k] = rowInt[k];
-                    }
-                    dataCounter++;
-                }
-                else
-                {
-                    rowInt[1] = i + 1;
-                    rowInt[2] = j + 1;
-                    rowInt[3] = defaultValueWhenNotFound;
+            fullData.Add(currentData);
 
-                    for (int k = 0; k < row.Length; k++)
-                    {
-                        fullData[i, j, k] = rowInt[k];
-                    }
-                }
-            }
+            dataCounter++;
         }
+
+        Debug.Log($"The number of elements in fullData list is: {fullData.Count}.");
+
+        //for(int i = 0; i < dataWidth; i++)
+        //{
+        //    for (int j = 0; j < dataHeight; j++)
+        //    {
+        //        row = data[dataCounter].Split(new char[] { ',' });
+
+        //        // Converts row of string data from file into an int array
+        //        for (int k = 0; k < row.Length; k++)
+        //        {
+        //            int.TryParse(row[k], out rowInt[k]);
+        //        }
+
+        //        // Checks if current data row in file has coordinates which matchup with an array element 
+        //        // in the generated data array.
+        //        // If it matches, it assigns that entire row's data to that row in the data array and moves the process 
+        //        // forward to assign the next row of data from the file.
+        //        // If they do not match, it creates a new row of data with the coordinates not found in the 
+        //        // read file and assigns it a designated default "not found" value, and retains the same row 
+        //        // of data until it finds where to place it.
+        //        if(rowInt[1] == (i + 1) * dataIncrement && rowInt[2] == (j + 1) * dataIncrement)
+        //        {
+        //            for (int k = 0; k < row.Length; k++)
+        //            {
+        //                fullData[i, j, k] = rowInt[k];
+        //            }
+        //            dataCounter++;
+        //        }
+        //        else
+        //        {
+        //            rowInt[1] = i + 1;
+        //            rowInt[2] = j + 1;
+        //            rowInt[3] = defaultValueWhenNotFound;
+
+        //            for (int k = 0; k < row.Length; k++)
+        //            {
+        //                fullData[i, j, k] = rowInt[k];
+        //            }
+        //        }
+        //    }
+        //}
     }
 
     /*
@@ -133,13 +163,13 @@ public class CSVReader: MonoBehaviour
      * If they do not match, no data is assigned from the .csv file and it remains on the same line (waiting to find the matching 
      * node to assign its data to).
      */
-    public void CheckToAssignValue(Node node)
-    {
-        int arrayXIndex = node.gridX;
-        int arrayYIndex = node.gridY;
+    //public void CheckToAssignValue(Node node)
+    //{
+    //    int arrayXIndex = node.gridX;
+    //    int arrayYIndex = node.gridY;
 
-        node.Connectivity = fullData[arrayXIndex, arrayYIndex, 3];
-    }
+    //    node.Connectivity = fullData[arrayXIndex, arrayYIndex, 3];
+    //}
 
     /*
      * Solely provides a reference on a monobehaviour class to allow a button to access the static instance of DataRecorder which 
@@ -149,4 +179,29 @@ public class CSVReader: MonoBehaviour
     {
         DataRecorder.Instance.OutputPathData();
     }
+
+    #region Debugging
+
+    private void DebugListOutStringArray(string[] array, string debugMessageStart)
+    {
+        string message = debugMessageStart + "\n";
+
+        foreach (string item in array)
+        {
+            message += item + "\n";
+        }
+
+        //Debug.Log(message);
+        ExportDataToDebugLog(message);
+    }
+
+    public void ExportDataToDebugLog(string debugMessage)
+    {
+        string filePath = "Assets/Resources/DebugLog.txt";
+        StreamWriter file = new StreamWriter(filePath, true);
+
+        file.Write(debugMessage);
+        file.Close();
+    }
+    #endregion
 }
