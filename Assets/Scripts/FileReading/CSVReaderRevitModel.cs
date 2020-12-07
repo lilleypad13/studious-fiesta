b8e2dc638de15e3a6a6dff6f260d4e0656f5d75f
Steven Lilley <steven.b.lilley@gmail.com>
2020-12-07 17:09:19 -0500

    CSVReaderRevitModel gives access to an array of string through the Inspector which lets the user put in the names of various sheets (.csv files) from a Revit parameter Excel data file and then reads that data into separate 2D string arrays which are placed into a dictionary (key = string sheet name; value = entire 2D string array of data for a single sheet); Helps apply the newly created classes implementing the IRevitModelDataHandler interface by searching through sheets for key header terms to relay columns of interest so those classes know which values to associate with which objects in the model.

--- Assets/Scripts/FileReading/CSVReaderRevitModel.cs
+++ Assets/Scripts/FileReading/CSVReaderRevitModel.cs
@@ -18,7 +18,7 @@ public class SheetWithColumnNames
     }
 }
 
-public class CSVReaderRevitModel : MonoBehaviour
+public class CSVReaderRevitModel : Initializer
 {
     private static CSVReaderRevitModel instance;
     private CSVReaderRevitModel()
@@ -46,29 +46,113 @@ public class CSVReaderRevitModel : MonoBehaviour
     [Tooltip("Name of folder in Unity's Resources folder which holds the .csv files corresponding to Revit models.")]
     [SerializeField] private string nameFolderRevitModelCSVs = "RevitModelCSVs";
     [Tooltip("List of exact sheet names to read model data from")]
-    [SerializeField] private SheetWithColumnNames[] sheetsToRead;
+    [SerializeField] private string[] sheetsOfInterest;
 
-    private void TestCreateDataArray()
+    private Dictionary<string, string[,]> sheetDataDictionary = new Dictionary<string, string[,]>();
+
+
+    public override void Initialization()
     {
-        List<string[,]> allDataArrays = new List<string[,]>();
+        sheetDataDictionary = CreateDataArraysFromCSVs();
 
-        foreach (SheetWithColumnNames item in sheetsToRead)
+        if (sheetsOfInterest == null)
+            Debug.LogWarning("No Data was read in from Revit Data to apply to models.");
+        else
+            ApplyDoorData();
+
+        DebugDisplay2DArrayStringDictionary(sheetDataDictionary);
+    }
+
+
+    private Dictionary<string, string[,]> CreateDataArraysFromCSVs()
+    {
+        Dictionary<string, string[,]> allDataDictionary = new Dictionary<string, string[,]>();
+
+        foreach (string sheetName in sheetsOfInterest)
         {
-            string filePath = nameFolderRevitModelCSVs + "/" + item.SheetName;
+            string filePath = nameFolderRevitModelCSVs + "/" + sheetName;
 
-            allDataArrays.Add(CSVReader.Instance.ReadCSVFileTo2DStringArray(filePath));
+            allDataDictionary.Add(sheetName, CSVReader.Instance.ReadCSVFileTo2DStringArray(filePath));
+            Debug.Log("Added item to dictionary with key name: " + sheetName);
         }
 
+        return allDataDictionary;
+    }
+
+
+    private int FindColumnIndexWithString(string[] rowOfStrings, string searchTerm)
+    {
+        Debug.Log($"Find column index searched: {searchTerm} within the elements {ListOfArrayStrings(rowOfStrings)}.");
+
+        for (int i = 0; i < rowOfStrings.Length; i++)
+        {
+            Debug.Log($"Comparing: {searchTerm} &&& {rowOfStrings[i]}.");
+            if (rowOfStrings[i].Contains(searchTerm))
+            {
+                Debug.Log("FOUND RESULT.");
+                return i;
+            }
+        }
+
+        Debug.Log($"Could not find {searchTerm} in headers.");
+        return 0;
+    }
+    
+
+    private void ApplyDoorData()
+    {
+        string[,] data;
+        sheetDataDictionary.TryGetValue("Doors", out data);
+
+        string[] headingRow = new string[data.GetLength(0)];
+
+        for (int i = 0; i < data.GetLength(0); i++)
+        {
+            headingRow[i] = data[i, 0];
+        }
+
+        int columnIndex = FindColumnIndexWithString(headingRow, "DoorClosed");
+        RevitDoorDataHandler doorHandler = new RevitDoorDataHandler();
+
+        for (int i = 1; i < data.GetLength(1); i++)
+        {
+            GameObject modelToModify = GlobalModelData.Instance.SearchEntireModelForObjectWithNameContaining(data[0, i]);
+            doorHandler.ModifyModelWithData(modelToModify, data[columnIndex, i]);
+        }
+    }
+
+
+    #region Debugging
+
+    private void DebugDisplaySheetsData(List<string[,]> allDataArrays)
+    {
         int sheetCounter = 0;
         foreach (string[,] array in allDataArrays)
         {
-            CSVReader.Instance.DebugListOut2DArray(array, sheetsToRead[sheetCounter].SheetName);
+            CSVReader.Instance.DebugListOut2DArray(array, sheetsOfInterest[sheetCounter]);
             sheetCounter++;
         }
     }
 
-    private void Start()
+    private void DebugDisplay2DArrayStringDictionary(Dictionary<string, string[,]> dictionary)
     {
-        TestCreateDataArray();
+        foreach (KeyValuePair<string, string[,]> kvp in dictionary)
+        {
+            CSVReader.Instance.DebugListOut2DArray(kvp.Value, $"Key: {kvp.Key}");
+        }
     }
+
+    private string ListOfArrayStrings(string[] array)
+    {
+        string listAsSingleString = "";
+
+        foreach (string item in array)
+        {
+            listAsSingleString += item + " ";
+        }
+
+        return listAsSingleString;
+    }
+    
+    #endregion
 }
