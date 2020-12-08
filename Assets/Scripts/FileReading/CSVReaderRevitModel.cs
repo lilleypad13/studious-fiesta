f8f64dbae753a79415d640695624b6b18c5a61b3
Steven Lilley <steven.b.lilley@gmail.com>
2020-12-08 18:14:19 -0500

    Added helper class RevitModelDataHandlerManager to work with CSVReaderRevitModel; RevitModelDataHandlerManager helps by associating a specific string (that now only needs hardcoded in one area) with a specific type of IRevitModelDataHandler class; Then all data is searched by CSVReaderRevitModel and passed through RevitModelDataHandlerManager to determine how to apply the data; CSVReaderRevitModel made more general so it could deal with all types of data.

--- Assets/Scripts/FileReading/CSVReaderRevitModel.cs
+++ Assets/Scripts/FileReading/CSVReaderRevitModel.cs
@@ -4,17 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class SheetWithColumnNames
+public class SheetAndColumn
 {
-    [SerializeField]private string sheetName;
+    [SerializeField] private string sheetName;
     public string SheetName { get => sheetName; }
-    [SerializeField]private string[] columnNames;
-    public string[] ColumnNames { get => columnNames; }
+    [SerializeField] private string columnName;
+    public string ColumnName { get => columnName; }
 
-    public SheetWithColumnNames(string _sheetName, string[] _columnNames)
+    public SheetAndColumn(string _sheetName, string _columnName)
     {
         sheetName = _sheetName;
-        columnNames = _columnNames;
+        columnName = _columnName;
     }
 }
 
@@ -46,9 +46,9 @@ public class CSVReaderRevitModel : Initializer
     [Tooltip("Name of folder in Unity's Resources folder which holds the .csv files corresponding to Revit models.")]
     [SerializeField] private string nameFolderRevitModelCSVs = "RevitModelCSVs";
     [Tooltip("List of exact sheet names to read model data from")]
-    [SerializeField] private string[] sheetsOfInterest;
+    [SerializeField] private SheetAndColumn[] sheetsOfInterest;
 
-    private Dictionary<string, string[,]> sheetDataDictionary = new Dictionary<string, string[,]>();
+    private static Dictionary<string, string[,]> sheetDataDictionary = new Dictionary<string, string[,]>();
 
 
     public override void Initialization()
@@ -58,7 +58,7 @@ public class CSVReaderRevitModel : Initializer
         if (sheetsOfInterest == null)
             Debug.LogWarning("No Data was read in from Revit Data to apply to models.");
         else
-            ApplyDoorData();
+            ApplyDataToModel();
 
         DebugDisplay2DArrayStringDictionary(sheetDataDictionary);
     }
@@ -68,12 +68,12 @@ public class CSVReaderRevitModel : Initializer
     {
         Dictionary<string, string[,]> allDataDictionary = new Dictionary<string, string[,]>();
 
-        foreach (string sheetName in sheetsOfInterest)
+        foreach (SheetAndColumn sheetName in sheetsOfInterest)
         {
-            string filePath = nameFolderRevitModelCSVs + "/" + sheetName;
+            string filePath = nameFolderRevitModelCSVs + "/" + sheetName.SheetName;
 
-            allDataDictionary.Add(sheetName, CSVReader.Instance.ReadCSVFileTo2DStringArray(filePath));
-            Debug.Log("Added item to dictionary with key name: " + sheetName);
+            allDataDictionary.Add(sheetName.SheetName, CSVReader.Instance.ReadCSVFileTo2DStringArray(filePath));
+            Debug.Log("Added item to dictionary with key name: " + sheetName.SheetName);
         }
 
         return allDataDictionary;
@@ -97,27 +97,47 @@ public class CSVReaderRevitModel : Initializer
         Debug.Log($"Could not find {searchTerm} in headers.");
         return 0;
     }
-    
 
-    private void ApplyDoorData()
+
+    private int SearchHeadersForColumnIndex(string[,] data, string[] headingRow, string headerSearchingFor)
     {
-        string[,] data;
-        sheetDataDictionary.TryGetValue("Doors", out data);
-
-        string[] headingRow = new string[data.GetLength(0)];
-
         for (int i = 0; i < data.GetLength(0); i++)
         {
             headingRow[i] = data[i, 0];
         }
 
-        int columnIndex = FindColumnIndexWithString(headingRow, "DoorClosed");
-        RevitDoorDataHandler doorHandler = new RevitDoorDataHandler();
+        int columnIndex = FindColumnIndexWithString(headingRow, headerSearchingFor);
+        return columnIndex;
+    }
 
-        for (int i = 1; i < data.GetLength(1); i++)
+
+    public static string[,] FindWithinDictionary(string searchTerm)
+    {
+        string[,] array;
+        sheetDataDictionary.TryGetValue(searchTerm, out array);
+
+        return array;
+    }
+    
+
+    private void ApplyDataToModel()
+    {
+        foreach(SheetAndColumn item in sheetsOfInterest)
         {
-            GameObject modelToModify = GlobalModelData.Instance.SearchEntireModelForObjectWithNameContaining(data[0, i]);
-            doorHandler.ModifyModelWithData(modelToModify, data[columnIndex, i]);
+            Debug.Log($"CSVReaderRevitModel trying to apply data based on sheet: {item.SheetName}.");
+            string[,] data = FindWithinDictionary(item.SheetName);
+            string[] headingRow = new string[data.GetLength(0)];
+            int columnIndex = SearchHeadersForColumnIndex(data, headingRow, item.ColumnName);
+
+            for (int i = 1; i < data.GetLength(1); i++)
+            {
+                string idNumber = data[0, i];
+                GameObject modelToModify = GlobalModelData.Instance.SearchEntireModelForObjectWithNameContaining(idNumber);
+                if (modelToModify != null)
+                    RevitModelDataHandlerManager.Instance.ApplyHandlerMethodBasedOnString(item.SheetName, modelToModify, data[columnIndex, i]);
+                else
+                    Debug.Log($"Did not find an object within the model associated with ID: {idNumber}.");
+            }
         }
     }
 
@@ -129,7 +149,7 @@ public class CSVReaderRevitModel : Initializer
         int sheetCounter = 0;
         foreach (string[,] array in allDataArrays)
         {
-            CSVReader.Instance.DebugListOut2DArray(array, sheetsOfInterest[sheetCounter]);
+            CSVReader.Instance.DebugListOut2DArray(array, sheetsOfInterest[sheetCounter].SheetName);
             sheetCounter++;
         }
     }
---
+++ Assets/Scripts/FileReading/RevitModelDataHandlerManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class RevitModelDataHandlerManager
+{
+    private static RevitModelDataHandlerManager instance;
+    private RevitModelDataHandlerManager()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+
+        instance = this;
+    }
+    public static RevitModelDataHandlerManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                new RevitModelDataHandlerManager();
+            }
+            return instance;
+        }
+    }
+
+    private static RevitDoorDataHandler doorHandler = new RevitDoorDataHandler();
+    private static RevitWallDataHandler wallHandler = new RevitWallDataHandler();
+
+    private static Dictionary<string, IRevitModelDataHandler> revitHandlerDictionary = new Dictionary<string, IRevitModelDataHandler>() {
+        ["Doors"] = doorHandler,
+        ["Walls"] = wallHandler
+    };
+
+    public void ApplyHandlerMethodBasedOnString(
+        string handlerIdentifier, GameObject objectToModify, string valueForModification)
+    {
+        Debug.Log($"DataHandlerManager attempting to modify {objectToModify.name} with value {valueForModification} using handlerID: {handlerIdentifier}.");
+        IRevitModelDataHandler dataHandler;
+        revitHandlerDictionary.TryGetValue(handlerIdentifier, out dataHandler);
+
+        if (dataHandler != null)
+            dataHandler.ModifyModelWithData(objectToModify, valueForModification);
+        else
+            Debug.LogWarning($"DataHandlerManager did not find a DataHandler associated with the term: {handlerIdentifier}.");
+    }
+}
