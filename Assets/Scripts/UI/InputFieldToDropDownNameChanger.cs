1219b62403112a9e4eb895d6e54cb7412e655caa
Steven Lilley <steven.b.lilley@gmail.com>
2021-02-15 16:56:09 -0500

    New class to provide interaction between an InputField and Dropdown menus so a Button can be used to change the name of a dropdown to match that within the InputField.

---
+++ Assets/Scripts/UI/InputFieldToDropDownNameChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class InputFieldToDropDownNameChanger : MonoBehaviour
+{
+    public DropdownManager[] dropdownManagers;
+    private InputField inputField;
+
+    private void Start()
+    {
+        inputField = GetComponent<InputField>();
+    }
+
+    public void RenameDropdownItem(int managersArrayIndex)
+    {
+        if(!string.IsNullOrEmpty(inputField.text))
+            dropdownManagers[managersArrayIndex].RenameCurrentDropDownItemText(inputField.text);
+    }
+}
